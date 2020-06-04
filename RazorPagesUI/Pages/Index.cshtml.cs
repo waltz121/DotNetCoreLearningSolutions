24cd9ec23b31cc1902769bea67567e288dec28bb
Walter R. Robledo <robleo.walterrobbin@gmail.com>
2020-06-04 15:58:07 +0800

    Add project files.

---
+++ RazorPagesUI/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace RazorPagesUI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public string FirstName { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
